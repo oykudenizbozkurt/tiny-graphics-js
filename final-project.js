9a9aff626f5a572fe797ff878e8cbb8759797cde
Deniz Bozkurt <oykudenizbozkurt@gmail.com>
2021-03-06 19:11:07 -0800

    Basic Scene

---
+++ final-project.js
@@ -0,0 +1,55 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
+} = tiny;
+
+const {Cube, Axis_Arrows, Textured_Phong} = defs
+
+export class Final extends Scene {
+    /**
+     *  **Base_scene** is a Scene that can be added to any display canvas.
+     *  Setup the shapes, materials, camera, and lighting here.
+     */
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+        this.shapes = {
+            box_1: new Cube(),
+        }
+
+        this.materials = {
+            phong: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+            }),
+        }
+
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+    }
+
+    make_control_panel() {
+        this.key_triggered_button("Rotate", ["c"], () => {
+            this.rotate ^= 1;
+        });
+    }
+
+    display(context, program_state) {
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(Mat4.translation(0, 0, -5));
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        const light_position = vec4(10, 10, 10, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+
+        let t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        let model_transform = Mat4.identity();
+        // this.shapes.axis.draw(context, program_state, model_transform, this.materials.phong.override({color: hex_color("#ffff00")}));
+        this.shapes.box_1.draw(context, program_state, model_transform, this.materials.phong);
+
+    }
+}
